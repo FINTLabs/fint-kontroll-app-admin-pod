bfd4f52e1703d702b7d0a96f02fad2e9cb1074ed
“Christian-R-Haugland” <christian.haugland@itverket.no>
2023-10-04 12:38:21 +0200

    Test of build

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect} from 'react';
 import {Button, Table} from "@navikt/ds-react";
 
-interface User {
-    name: String
-}
+// interface User {
+//     name: String
+// }
 
 function App() {
-    const [isLoading, setIsLoading] = useState(true)
-    const [users, setUsers] = useState<User[]>([])
+    // const [isLoading, setIsLoading] = useState(true)
+    // const [users, setUsers] = useState<User[]>([])
 
     useEffect(() => {
-        const res = fetch('api/users').then(res => {
-            console.log("Hallos")
-            console.log(res)
-        }).catch(e => {
-            console.log(e)
-        }).finally(() => setIsLoading(false))
+        // const res = fetch('api/users').then(res => {
+        //     console.log("Hallos")
+        //     console.log(res)
+        // }).catch(e => {
+        //     console.log(e)
+        // }).finally(() => setIsLoading(false))
     }, [])
 
-    if(isLoading) {
-        return <div>Loading...</div>
-    }
+    // if(isLoading) {
+    //     return <div>Loading...</div>
+    // }
 
     return (
         <div className="App">
@@ -32,8 +32,7 @@ function App() {
                 <Table.Header>
                     <Table.Row>
                         <Table.HeaderCell scope="col">Navn</Table.HeaderCell>
-                        <Table.HeaderCell scope="col">Fødseslnr.</Table.HeaderCell>
-                        <Table.HeaderCell scope="col">Start</Table.HeaderCell>
+                        <Table.HeaderCell scope="col">Dato</Table.HeaderCell>
                     </Table.Row>
                 </Table.Header>
                 <Table.Body>
