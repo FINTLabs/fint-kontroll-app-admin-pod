af1cd1665634a201af03e6b24358f18e8b2b1a1e
“Christian-R-Haugland” <christian.haugland@itverket.no>
2023-10-04 12:33:50 +0200

    Podlet fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,50 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {Button, Table} from "@navikt/ds-react";
+
+interface User {
+    name: String
+}
 
 function App() {
+    const [isLoading, setIsLoading] = useState(true)
+    const [users, setUsers] = useState<User[]>([])
+
+    useEffect(() => {
+        const res = fetch('api/users').then(res => {
+            console.log("Hallos")
+            console.log(res)
+        }).catch(e => {
+            console.log(e)
+        }).finally(() => setIsLoading(false))
+    }, [])
+
+    if(isLoading) {
+        return <div>Loading...</div>
+    }
+
     return (
         <div className="App">
             <header className="App-header">
-                Test header
+                <Button>Ba bom bi bom</Button>
             </header>
+
+            <Table>
+                <Table.Header>
+                    <Table.Row>
+                        <Table.HeaderCell scope="col">Navn</Table.HeaderCell>
+                        <Table.HeaderCell scope="col">Fødseslnr.</Table.HeaderCell>
+                        <Table.HeaderCell scope="col">Start</Table.HeaderCell>
+                    </Table.Row>
+                </Table.Header>
+                <Table.Body>
+                    <Table.DataCell>HALO</Table.DataCell>
+                    {/*{users.map((user: User, i) => {*/}
+                    {/*    <Table.Row key={i}>*/}
+                    {/*        <Table.DataCell>{user.name}</Table.DataCell>*/}
+                    {/*    </Table.Row>*/}
+                    {/*})}*/}
+                </Table.Body>
+            </Table>
         </div>
     );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,13 @@
+import "@navikt/ds-css";
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { makeServer } from "./miragejs/server"
+
+
+if (process.env.NODE_ENV === "development") {
+    makeServer({ environment: "development" })
+}
 
 const root = ReactDOM.createRoot(
     document.getElementById('fint-kontroll-app-admin-pod') as HTMLElement
@@ -10,3 +17,6 @@ root.render(
         <App/>
     </React.StrictMode>
 );
+
+
+export default App;
