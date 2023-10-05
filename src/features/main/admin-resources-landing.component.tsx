972ad14cba85c877d8f086f2f12742086208e050
“Christian-R-Haugland” <christian.haugland@itverket.no>
2023-10-05 16:46:40 +0200

    Typescript config updated, loader implemented, condition created for Resources

--- src/features/main/admin-resources-landing.component.tsx
+++ src/features/main/admin-resources-landing.component.tsx
@@ -1,32 +1,12 @@
 import {ResourcesTableComponent} from "../resources/resources-table.component";
-import React, {useEffect, useState} from "react";
-import {Resource} from "@utils/types";
+import React from "react";
 import {ResourcesHeaderComponent} from "../resources/information-header.component";
 
 export const AdminResourcesLandingComponent = () => {
-    const [isLoading, setIsLoading] = useState(true)
-    const [resources, setResources] = useState<Resource[]>([])
-
-    useEffect(() => {
-        getResources()
-        setIsLoading(false)
-    }, []);
-
-    const getResources = () => {
-        fetch('api/resources')
-            .then(res => res.json())
-            .then((data) => setResources(data.resources))
-            .catch(e => console.log(e)) // TODO: Implement error handling
-    }
-
-    if(isLoading) {
-        return <div>Loading...</div>
-    }
-
     return (
         <div>
             <ResourcesHeaderComponent />
-            <ResourcesTableComponent resources={resources} />
+            <ResourcesTableComponent />
         </div>
     )
 }
--- src/features/resources/resources-table.component.tsx
+++ src/features/resources/resources-table.component.tsx
@@ -1,12 +1,33 @@
-import {Table} from "@navikt/ds-react";
-import React from "react";
+import {Loader, Table} from "@navikt/ds-react";
+import React, {useEffect, useState} from "react";
 import { InformationSquareIcon } from '@navikt/aksel-icons';
 import {Resource} from "@utils/types";
+import styled from "styled-components";
 
-interface ResourcesTableComponentProps {
-    resources: Resource[]
-}
-export const ResourcesTableComponent = (props: ResourcesTableComponentProps) =>  {
+const TableDatacellStyled = styled(Table.DataCell)`
+    width: 100%;
+  
+    position: absolute;
+    display: flex;
+    justify-content: center;
+`
+
+export const ResourcesTableComponent = () =>  {
+    const [isLoading, setIsLoading] = useState(true)
+    const [resources, setResources] = useState<Resource[]>([])
+
+    useEffect(() => {
+        getResources()
+
+    }, []);
+    const getResources = () => {
+        fetch('api/resources')
+            .then(res => res.json())
+            .then((data) => setResources(data.resources))
+            .catch(e => console.log(e)) // TODO: Implement error handling
+            .finally(() => setIsLoading(false))
+    }
+
     return (
         <Table>
             <Table.Header>
@@ -19,21 +40,29 @@ export const ResourcesTableComponent = (props: ResourcesTableComponentProps) => 
                 </Table.Row>
             </Table.Header>
             <Table.Body>
-                {props.resources.map((resource: Resource, i) => {
-                    return (
-                        <Table.Row key={i}>
+            {isLoading
+                ?
+                    <TableDatacellStyled colSpan={5} >
+                        <Loader size="3xlarge" title="Laster" className="loader" />
+                    </TableDatacellStyled>
+                :
+
+                    resources.map((resource: Resource, i) => {
+                        return (
+                            <Table.Row key={i}>
                             <Table.DataCell>{resource.resourceName}</Table.DataCell>
                             <Table.DataCell>{resource.resourceType}</Table.DataCell>
                             <Table.DataCell>{resource.resourceLimit}</Table.DataCell>
                             <Table.DataCell>{resource.resourceLimit}</Table.DataCell>
                             <Table.DataCell>
-                                <a href={`/${resource.id}`} className="flex-center-vertically">
+                                <a href={`/info/${resource.id}`} className="flex-center-vertically">
                                     Se detaljer <InformationSquareIcon className="margin-left-1-x" />
                                 </a>
                             </Table.DataCell>
                         </Table.Row>
-                    )
-                })}
+                        )
+                    })
+            }
             </Table.Body>
         </Table>
     )
