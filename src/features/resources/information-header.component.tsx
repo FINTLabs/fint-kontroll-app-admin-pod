82c4e554d003950106aad9a555886c04af3c27f0
“Christian-R-Haugland” <christian.haugland@itverket.no>
2023-10-05 16:02:59 +0200

    Component missed by linter implemented. More styles

--- src/features/resources/information-header.component.tsx
+++ src/features/resources/information-header.component.tsx
@@ -4,17 +4,25 @@ import styled from "styled-components";
 const ResourcesInfoWrapper = styled.div`
     display: flex;
     justify-content: space-between;
+    align-items: center;
+    
+    .flex {
+        display: flex;
+        gap: 1rem;
+    }
 `
 
 export const ResourcesHeaderComponent = () => {
     return (
         <ResourcesInfoWrapper>
             <h2>Ressursadmin</h2>
-            <div>
-                <Search label="Søk her" />
-            </div>
-            <div>
-                <Button>Velg enhet</Button>
+            <div className="flex">
+                <div>
+                    <Search label="Søk her" />
+                </div>
+                <div>
+                    <Button>Velg enhet</Button>
+                </div>
             </div>
         </ResourcesInfoWrapper>
     )
