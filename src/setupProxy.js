f47413f1f6161b9d41c4f92acf77b2c851b8b18b
“Christian-R-Haugland” <christian.haugland@itverket.no>
2023-10-09 10:02:55 +0200

    Proxy created

---
+++ src/setupProxy.js
@@ -0,0 +1,54 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function (app) {
+    app.use(
+        createProxyMiddleware('/api/resources', {
+            target: 'http://localhost:8120/beta/fintlabs-no', // API endpoint 1
+            changeOrigin: true,
+            //pathRewrite: {"^/api1": ""},
+            headers: {
+                Connection: "keep-alive"
+            }
+        })
+    );
+    // app.use(
+    //     createProxyMiddleware('/api/orgunits', {
+    //         target: 'http://localhost:8081', // API endpoint 5
+    //         changeOrigin: true,
+    //         //pathRewrite: {"^/api5": ""},
+    //         headers: {
+    //             Connection: "keep-alive"
+    //         }
+    //     })
+    // );
+    /*app.use(
+        createProxyMiddleware('/api/users', {
+            target: 'http://localhost:8080',
+            changeOrigin: true,
+            //pathRewrite: {"^/api5": ""},
+            headers: {
+                Connection: "keep-alive"
+            }
+        })
+    );*/
+    // app.use(
+    //     createProxyMiddleware('/api/users', {
+    //         target: 'http://localhost:8062/beta/fintlabs-no', // API endpoint 1
+    //         changeOrigin: true,
+    //         //pathRewrite: {"^/api1": ""},
+    //         headers: {
+    //             Connection: "keep-alive"
+    //         }
+    //     })
+    // );
+    // app.use(
+    //     createProxyMiddleware('/api/assignments', {
+    //         target: 'http://localhost:8097',
+    //         changeOrigin: true,
+    //         //pathRewrite: {"^/api5": ""},
+    //         headers: {
+    //             Connection: "keep-alive"
+    //         }
+    //     })
+    // );
+}
