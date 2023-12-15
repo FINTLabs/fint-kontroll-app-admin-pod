ef6dd5999c2c35b182a96e7472ee47be6a5258f3
“Christian-R-Haugland” <christian.haugland@itverket.no>
2023-12-15 12:38:07 +0100

    Added gap

--- src/features/adminResourceInfo/index.tsx
+++ src/features/adminResourceInfo/index.tsx
@@ -1,13 +1,19 @@
 import { Heading } from "@navikt/ds-react";
 import { AdminResourcesTable } from "./adminResourcesTable";
+import styled from "styled-components";
 
+const AdminResourceContainer = styled.div`
+	display: flex;
+	gap: 1rem;
+	flex-direction: column;
+`;
 export const AdminResourceInfo = () => {
 	return (
-		<div>
+		<AdminResourceContainer>
 			<Heading size={"large"} level={"2"}>
 				Applikasjonsadministrasjon
 			</Heading>
 			<AdminResourcesTable />
-		</div>
+		</AdminResourceContainer>
 	);
 };
