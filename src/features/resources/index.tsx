35111112d9b48e7ce9777cfeda7fef2fdeb29bcf
“Christian-R-Haugland” <christian.haugland@itverket.no>
2023-12-15 12:59:28 +0100

    Fixed bug causing the resource not to be loaded

--- src/features/resources/index.tsx
+++ src/features/resources/index.tsx
@@ -1,38 +1,45 @@
 import { ResourceDetails } from "./resourceDetails";
-import {useContext, useEffect} from "react";
-import {useParams} from "react-router-dom";
-import {ResourceContext} from "../../Context/resourcesContext";
-import {ResourceInfoOrgUnitTable} from "./infoOrgUnitTable";
+import { useContext, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { ResourceContext } from "../../Context/resourcesContext";
+import { ResourceInfoOrgUnitTable } from "./infoOrgUnitTable";
 import styled from "styled-components";
-import {useGeneral} from "../../Context";
+import { useGeneral } from "../../Context";
+import { LoaderStyled } from "../../atoms/LoaderCentered";
 
 const ResourceInfoContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    gap: 2rem;
-`
+	display: flex;
+	flex-direction: column;
+	gap: 2rem;
+`;
 
 export const ResourceInfo = () => {
-    const {basePath} = useGeneral()
-    const {getResourceById, resourceDetails} = useContext(ResourceContext)
-    const {id} = useParams<string>()
+	const { basePath } = useGeneral();
+	const { getResourceById, resourceDetails, isLoading } =
+		useContext(ResourceContext);
+	const { id } = useParams<string>();
 
+	useEffect(() => {
+		if (id) {
+			getResourceById(basePath, id);
+		}
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [basePath, id]);
 
+	if (!resourceDetails) {
+		return null;
+	}
 
-    useEffect(() => {
-        if(id && !resourceDetails) {
-            getResourceById(basePath, id)
-        }
-    }, [basePath, getResourceById, id, resourceDetails])
+	if (isLoading) {
+		return <LoaderStyled size={"3xlarge"} />;
+	}
 
-    if(!resourceDetails) {
-        return null
-    }
-
-    return (
-        <ResourceInfoContainer>
-            <ResourceDetails resourceDetails={resourceDetails} />
-            <ResourceInfoOrgUnitTable validForOrgUnits={resourceDetails.validForOrgUnits} />
-        </ResourceInfoContainer>
-    )
-}
+	return (
+		<ResourceInfoContainer>
+			<ResourceDetails resourceDetails={resourceDetails} />
+			<ResourceInfoOrgUnitTable
+				validForOrgUnits={resourceDetails.validForOrgUnits}
+			/>
+		</ResourceInfoContainer>
+	);
+};
