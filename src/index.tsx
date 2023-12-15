2de3b53150429b87ee764411e2f41544e0ac50ce
“Christian-R-Haugland” <christian.haugland@itverket.no>
2023-12-15 10:50:57 +0100

    Removed mirage from project

--- src/index.tsx
+++ src/index.tsx
@@ -1,32 +1,29 @@
 import "@navikt/ds-css";
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import { makeServer } from "./miragejs/server"
-import {BrowserRouter} from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 import GlobalStyle from "./global-styles";
-import {GeneralProvider} from "./Context";
-import {ToastContainer} from "react-toastify";
-
-
-if (process.env.LOCAL_TEST === 'true') {
-    makeServer({ environment: "development" })
-}
+import { GeneralProvider } from "./Context";
+import { ToastContainer } from "react-toastify";
 
 const root = ReactDOM.createRoot(
-    document.getElementById('fint-kontroll-app-admin-pod') as HTMLElement
+	document.getElementById("fint-kontroll-app-admin-pod") as HTMLElement,
 );
 root.render(
-    <React.StrictMode>
-        <BrowserRouter>
-            <GeneralProvider>
-                <ToastContainer autoClose={5000} newestOnTop={true} role="alert" />
-                <GlobalStyle />
-                <App/>
-            </GeneralProvider>
-        </BrowserRouter>
-    </React.StrictMode>
+	<React.StrictMode>
+		<BrowserRouter>
+			<GeneralProvider>
+				<ToastContainer
+					autoClose={5000}
+					newestOnTop={true}
+					role="alert"
+				/>
+				<GlobalStyle />
+				<App />
+			</GeneralProvider>
+		</BrowserRouter>
+	</React.StrictMode>,
 );
-
 
 export default App;
